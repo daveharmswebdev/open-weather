f48fc2a311546ce9bcac05cd67d023d6fab77be1
Dave Harms <daveharmswebdev@gmail.com>
2016-07-18 15:09:06 -0500

    added fb-getter and firebaseconfig modules

---
+++ src/fb-getter.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function getKey() {
+  return {
+    key: "AIzaSyBzplHZe3S3znD8h2HGWDEqpLN5xURFtHw",
+    url: "https://music-history-7af37.firebaseio.com",
+    authurl: "music-history-7af37.firebaseapp.com",
+    bucketUrl: "music-history-7af37.appspot.com"
+  };
+}
+
+module.exports = getKey;
